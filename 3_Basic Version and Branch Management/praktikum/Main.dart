f58ca6936cd7a981d795a364befac59e344c4940
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-08-23 16:49:36 +0700

    Task 1 Pembuatan Repository

--- 3_Basic Version and Branch Management/praktikum/Main.dart
+++ 3_Basic Version and Branch Management/praktikum/Main.dart
@@ -1,10 +1,3 @@
 main() {
   print("Hello World");
-  print("Feature A");
-  print("Feature B");
-  print("Feature F");
-  print("Feature C");
-  print("Feature D");
-  print("Feature E");
-  print("Feature D");
 }
