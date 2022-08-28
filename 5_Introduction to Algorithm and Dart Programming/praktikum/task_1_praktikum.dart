5310a2d8cd76ef8049048d6e16aaba122826d9ab
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-08-28 17:23:51 +0700

    Add Folder Section 5

---
+++ 5_Introduction to Algorithm and Dart Programming/praktikum/task_1_praktikum.dart
@@ -0,0 +1,5 @@
+void main() {
+  double pi = 3.14;
+  int r = 10;
+  print(pi * r * r);
+}
---
+++ 5_Introduction to Algorithm and Dart Programming/praktikum/task_2_praktikum.dart
@@ -0,0 +1,7 @@
+void main() {
+  String nama = 'Nama';
+  String nama_dpn = 'Nico';
+  String nama_blkng = 'Prasetyo';
+
+  print('$nama' ' : ' '$nama_dpn' ' ' '$nama_blkng');
+}
