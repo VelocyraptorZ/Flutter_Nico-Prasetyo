c5477d20e2fab6d6f731424dd0c362b040f7eb54
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-08-30 09:59:21 +0700

    Praktikum Section 5 dan 6

--- 5_Introduction to Algorithm and Dart Programming/praktikum/task_1_praktikum.dart
+++ 5_Introduction to Algorithm and Dart Programming/praktikum/task_1_praktikum.dart
@@ -2,5 +2,7 @@ void main() {
   double pi = 3.14;
   int r = 10;
   double hasil = pi * r * r;
-  print("Hasil = $hasil");
+
+  print("Jari -Jari Lingkaran = $r");
+  print("Luas Lingkaran = $hasil");
 }
