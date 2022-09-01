5f64c57f835aeb4f2ccd0df44b6d2849e5f48c61
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-09-01 15:09:52 +0700

    Praktikum Task 2 Section 8

---
+++ 8_Advance Function, Async-Await/praktikum/praktikum_task2.dart
@@ -0,0 +1,8 @@
+void main() {
+  // final colors = <String>['red', 'green', 'blue', 'orange', 'pink'];
+  // print(colors.sublist(1, 3)); // [green, blue]
+
+  List<String> sports = ['cricket', 'football', 'tennis', 'baseball'];
+  Map<int, String> map = sports.asMap();
+  print(map); // {0: cricket, 1: football, 2: tennis, 3: baseball}
+}
