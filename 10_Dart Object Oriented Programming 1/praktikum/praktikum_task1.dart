c059986ee44b7cc145cf60b5144e096c43cef561
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-09-06 18:04:55 +0700

    Latihan Praktikum Task 1 Section 10

--- 10_Dart Object Oriented Programming 1/praktikum/praktikum_task1.dart
+++ 10_Dart Object Oriented Programming 1/praktikum/praktikum_task1.dart
@@ -1,15 +1,25 @@
 class Hewan {
   int beratSapi = 70;
+  int beratKambing = 80;
 }
 
 class Mobil {
   var h1 = Hewan();
-  int kapasitas = 1000;
-  List<int> muatan = [];
+  int kapasitas = 100;
+  List muatan = [];
+  List<int> beratmuatan = [];
 
   void tambahMuatan() {
-    if (h1.beratSapi <= kapasitas) {
-      muatan.add(h1.beratSapi);
+    if (h1.beratSapi < kapasitas) {
+      muatan.add('Sapi');
+    } else if (h1.beratKambing < kapasitas) {
+      muatan.add('Kambing');
+    }
+  }
+
+  void totalMuatan() {
+    if (h1.beratSapi < kapasitas) {
+      beratmuatan.add(h1.beratSapi);
     }
   }
 }
@@ -18,7 +28,9 @@ void main() {
   var result = Mobil();
 
   result.tambahMuatan();
+  result.totalMuatan();
 
-  print('Kapasitas Muatan = ${result.kapasitas}');
+  print('Kapasitas Muatan Maximum = ${result.kapasitas}');
   print('Isi Muatan = ${result.muatan}');
+  print('Total Muatan = ${result.beratmuatan}');
 }
