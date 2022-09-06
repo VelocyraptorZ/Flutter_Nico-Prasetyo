e9aebb6a57a02d848bd4dc614a0398ccfdce84fc
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-09-07 01:36:00 +0700

    Praktikum Task 1 dan Task 2 Section 10

--- 10_Dart Object Oriented Programming 1/praktikum/praktikum_task1_task2.dart
+++ 10_Dart Object Oriented Programming 1/praktikum/praktikum_task1_task2.dart
@@ -1,36 +1,102 @@
+// class Hewan {
+//   int beratSapi = 70;
+//   int beratKambing = 80;
+// }
+
+// class Mobil {
+//   var h1 = Hewan();
+//   int kapasitas = 100;
+//   List muatan = [];
+//   List<int> beratmuatan = [];
+
+//   void tambahMuatan() {
+//     if (h1.beratSapi < kapasitas) {
+//       muatan.add('Sapi');
+//       muatan.add('Kambing');
+//       beratmuatan.add(h1.beratSapi);
+//       beratmuatan.add(h1.beratKambing);
+//     }
+//   }
+
+//   int totalMuatan() {
+//     int total = 0;
+//     for (int hitung in beratmuatan) {
+//       total += hitung;
+//     }
+//     return total;
+//   }
+// }
+
+// void main() {
+//   var result = Mobil();
+
+//   result.tambahMuatan();
+//   result.totalMuatan();
+
+//   print('Kapasitas Muatan Maximum = ${result.kapasitas}');
+//   print('Isi Muatan = ${result.muatan}');
+//   print('Total Muatan = ${result.totalMuatan()}');
+// }
+
+import 'dart:io';
+
 class Hewan {
-  int beratSapi = 70;
-  int beratKambing = 80;
+  List<int> beratMuatan = [];
 }
 
 class Mobil {
-  var h1 = Hewan();
+  var berat = Hewan();
   int kapasitas = 100;
-  List muatan = [];
-  List<int> beratmuatan = [];
+  List<String> muatan = [];
 
-  void tambahMuatan() {
-    if (h1.beratSapi < kapasitas) {
-      muatan.add('Sapi');
-    } else if (h1.beratKambing < kapasitas) {
-      muatan.add('Kambing');
+  int tambahMuatan(int berathewan, String namahewan) {
+    if (berathewan <= kapasitas) {
+      berat.beratMuatan.add(berathewan);
+      muatan.add(namahewan);
+      kapasitas -= berathewan;
+    } else {
+      print('Melebihi Kapasitas');
     }
+    return 0;
   }
 
-  void totalMuatan() {
-    if (h1.beratSapi < kapasitas) {
-      beratmuatan.add(h1.beratSapi);
+  int totalMuatan() {
+    int total = 0;
+    for (int hitung in berat.beratMuatan) {
+      total += hitung;
     }
+    return total;
   }
 }
 
 void main() {
   var result = Mobil();
+  bool ulang = true;
 
-  result.tambahMuatan();
-  result.totalMuatan();
+  do {
+    String jawab = '';
 
-  print('Kapasitas Muatan Maximum = ${result.kapasitas}');
-  print('Isi Muatan = ${result.muatan}');
-  print('Total Muatan = ${result.beratmuatan}');
+    stdout.write('Masukan Nama Hewan  = ');
+    String? namahewan = (stdin.readLineSync()!);
+
+    stdout.write('Masukan Berat Hewan = ');
+    int? berathewan = int.parse(stdin.readLineSync()!);
+    result.tambahMuatan(berathewan, namahewan);
+
+    print('Daftar Muatan               = ${result.muatan}');
+    print('Daftar Berat Muatan         = ${result.berat.beratMuatan}');
+    print('Sisa Kapasitas Muatan Mobil = ${result.kapasitas}');
+    print('Total Berat Muatan          = ${result.totalMuatan()}');
+
+    do {
+      stdout.write('Isi Data Muatan Lagi (y/n) ? ');
+      jawab = stdin.readLineSync()!;
+
+      if (jawab == 'n') {
+        exit(0);
+      } else if (jawab != 'y') {
+        print('Error Input');
+      }
+    } while (jawab != 'y');
+  } while (ulang == true);
 }
