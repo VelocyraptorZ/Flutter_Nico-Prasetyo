334fcc51a0afe8ea8d4856e59e5ca774d258e74c
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-09-16 21:18:18 +0700

    Tes

--- 13_Platform Widget/praktikum/praktikum_task2/lib/main.dart
+++ 13_Platform Widget/praktikum/praktikum_task2/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,59 +10,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return const CupertinoApp(
+        title: 'Flutter Demo',
+        theme: CupertinoThemeData(),
+        home: CupertinoPageScaffold(
+          navigationBar: CupertinoNavigationBar(
+            leading: Text('Edit'),
+            trailing: Icon(Icons.edit),
+            middle: Text('Yolo'),
+          ),
+          child: Center(
+            child: CupertinoSearchTextField(),
+          ),
+        ));
   }
 }
