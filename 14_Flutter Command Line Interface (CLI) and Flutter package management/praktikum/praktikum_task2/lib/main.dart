3807f21fa5cf35638464a31b6bb038ccb12021c0
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-09-20 17:32:31 +0700

    Praktikum Task 2

--- 14_Flutter Command Line Interface (CLI) and Flutter package management/praktikum/praktikum_task2/lib/main.dart
+++ 14_Flutter Command Line Interface (CLI) and Flutter package management/praktikum/praktikum_task2/lib/main.dart
@@ -1,85 +1,56 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_barcodes/barcodes.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyBarcode(),
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
-class MyBarcode extends StatefulWidget {
-  const MyBarcode({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-  @override
-  State<MyBarcode> createState() => _MyBarcodeState();
-}
-
-class _MyBarcodeState extends State<MyBarcode> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Barcodes'),
+        title: const Text('Flutter Barcode'),
         centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            barcode1(),
-            barcode2(),
-            barcode3(),
+          children: [
+            BarcodeWidget(
+              padding: const EdgeInsets.all(10),
+              barcode: Barcode.code128(),
+              data: 'Alterra Academy',
+              width: 200,
+              height: 150,
+            ),
+            BarcodeWidget(
+              padding: const EdgeInsets.all(10),
+              barcode: Barcode.code128(),
+              data: 'Flutter Asik',
+              width: 200,
+              height: 150,
+            ),
+            BarcodeWidget(
+              padding: const EdgeInsets.all(10),
+              barcode: Barcode.code128(),
+              data: 'Nico Prasetyo',
+              width: 200,
+              height: 150,
+            ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget barcode1() {
-    return SizedBox(
-      height: 100,
-      width: 100,
-      child: SfBarcodeGenerator(
-        value: 'ALTERRA ACADEMY',
-        symbology: Code93(module: 2),
-        showValue: true,
-      ),
-    );
-  }
-
-  Widget barcode2() {
-    return SizedBox(
-      height: 100,
-      width: 100,
-      child: SfBarcodeGenerator(
-        value: 'FLUTER ASIK',
-        symbology: Code93(module: 2),
-        showValue: true,
-      ),
-    );
-  }
-
-  Widget barcode3() {
-    return SizedBox(
-      height: 100,
-      width: 100,
-      child: SfBarcodeGenerator(
-        value: 'NICO PRASETYO',
-        symbology: Code93(module: 2),
-        showValue: true,
       ),
     );
   }
