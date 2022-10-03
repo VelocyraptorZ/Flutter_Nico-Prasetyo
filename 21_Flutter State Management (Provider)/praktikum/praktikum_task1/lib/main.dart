7698826817033e839afc2917b98bbf608c00d838
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-10-03 23:02:54 +0700

    Praktikum Task 1 Section 21

--- 21_Flutter State Management (Provider)/praktikum/praktikum_task1/lib/main.dart
+++ 21_Flutter State Management (Provider)/praktikum/praktikum_task1/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Task Management With SetState',
       home: ContactScreen(),
     );
   }
