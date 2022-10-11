39bfefe94341ee8da2eed1b4bc1068082a566f92
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-10-11 14:25:10 +0700

    Praktikum Task 1 Section 24

--- 24_Storage/praktikum/praktikum_task1/lib/screens/register_screen.dart
+++ 24_Storage/praktikum/praktikum_task1/lib/screens/register_screen.dart
@@ -51,123 +51,126 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      padding: const EdgeInsets.all(16),
-      child: Form(
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        key: formKey,
-        child: ListView(
-          children: [
-            TextFormField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.account_circle_rounded),
-                labelText: 'Username',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value != null && value.length < 4) {
-                  return 'Enter at least 4 characters';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.email_rounded),
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-              validator: (email) {
-                if (email != null && !EmailValidator.validate(email)) {
-                  return 'Enter a valid email';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: _numberController,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.phone),
-                labelText: 'Phone',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value != null && value.length < 8) {
-                  return 'Enter correct phone number';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.password_rounded),
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value != null && value.length < 5) {
-                  return 'Enter min. 5 characters';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Provider.of<UserManager>(context, listen: false)
-                    .UserManagerRegister(
-                  _nameController.text.toString(),
-                  _emailController.text.toString(),
-                  _numberController.text.toString(),
-                  _passwordController.text.toString(),
-                );
-                final isValidForm = formKey.currentState!.validate();
-
-                String username = _nameController.text;
-                String email = _emailController.text;
-                String number = _numberController.text;
-                String password = _passwordController.text;
-
-                if (isValidForm) {
-                  registerdata.setBool('register', false);
-                  registerdata.setString('username', username);
-                  registerdata.setString('email', email);
-                  registerdata.setString('number', number);
-                  registerdata.setString('password', password);
-                  Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => HomePage(
-                          user: UserManager(),
-                        ),
-                      ),
-                      (route) => false);
-                }
-              },
-              child: const Text('register'),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Register'),
         ),
-      ),
-    ));
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            key: formKey,
+            child: ListView(
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.account_circle_rounded),
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value != null && value.length < 4) {
+                      return 'Enter at least 4 characters';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.email_rounded),
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (email) {
+                    if (email != null && !EmailValidator.validate(email)) {
+                      return 'Enter a valid email';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: _numberController,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.phone),
+                    labelText: 'Phone',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value != null && value.length < 8) {
+                      return 'Enter correct phone number';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.password_rounded),
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value != null && value.length < 5) {
+                      return 'Enter min. 5 characters';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Provider.of<UserManager>(context, listen: false)
+                        .UserManagerRegister(
+                      _nameController.text.toString(),
+                      _emailController.text.toString(),
+                      _numberController.text.toString(),
+                      _passwordController.text.toString(),
+                    );
+                    final isValidForm = formKey.currentState!.validate();
+
+                    String username = _nameController.text;
+                    String email = _emailController.text;
+                    String number = _numberController.text;
+                    String password = _passwordController.text;
+
+                    if (isValidForm) {
+                      registerdata.setBool('register', false);
+                      registerdata.setString('username', username);
+                      registerdata.setString('email', email);
+                      registerdata.setString('number', number);
+                      registerdata.setString('password', password);
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomePage(
+                              user: UserManager(),
+                            ),
+                          ),
+                          (route) => false);
+                    }
+                  },
+                  child: const Text('register'),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
